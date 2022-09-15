4f052019bf216f4483ef65acf33e0ef858446c46
rbeigh39 <rbeigh39@gmail.com>
2022-09-15 07:13:58 +0530

    Implemented Briefings

--- src/pages/Briefiings.js
+++ src/pages/Briefiings.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import TabHeader from "../components/TabHeader";
-import WorkInProgress from "./WorkInProgress";
+
+import classes from "../sass/pages/briefings.module.scss";
 
 const Briefings = () => {
+  const [timeTable, setTimeTable] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_BACKEND_URL}/api/v1/timeTable`,
+      withCredentials: true,
+    })
+      .then((res) => {
+        setTimeTable(() => [...res.data.data.timeTable[0].classes]);
+        // console.log(res.data.data.timeTable[0].classes);
+      })
+      .catch((err) => {
+        window.alert("something went wrong fetching time-table!");
+      });
+  }, []);
+
   return (
     <>
       <TabHeader title="Briefings" />
-      <WorkInProgress />
+
+      {timeTable.map((cur) => {
+        return (
+          <div className={classes["time-table__container"]} key={cur._id}>
+            <h4 className={classes["time-table__subject"]}>{cur.subject}</h4>
+            <p className={classes["time-table__time"]}>
+              Starts at: {cur.startTime}
+            </p>
+            <p className={classes["time-table__time"]}>
+              Ends at: {cur.endTime}
+            </p>
+          </div>
+        );
+      })}
+
+      {/* <WorkInProgress /> */}
     </>
   );
 };
