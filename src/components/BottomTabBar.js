1222c799e9b5475cd4265c9fa19d94b5cfdee65e
rbeigh39 <rbeigh39@gmail.com>
2022-09-22 10:55:38 +0530

    Minor Changes

--- src/components/BottomTabBar.js
+++ src/components/BottomTabBar.js
@@ -11,14 +11,6 @@ const BottomTabBar = () => {
   const [isActiveBriefings, setIsActiveBriefings] = useState(false);
   const [isActiveForum, setIsActiveForum] = useState(false);
 
-  console.log("these are the flags: ", {
-    isActiveHome,
-    isActiveNotification,
-    isActiveResources,
-    isActiveBriefings,
-    isActiveForum,
-  });
-
   const setActiveTarget = (target) => {
     setIsActiveHome(false);
     setIsActiveNotification(false);
